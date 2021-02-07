07b5d3637c852ad98ecc092d61d0ff477678b2f0
reemaljaber <reemaljaber1@gmail.com>
2021-02-07 21:22:24 +0300

    added saying_bye function

--- bikeshare.py
+++ bikeshare.py
@@ -306,7 +306,11 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
     breakfive()
-
+def saying_bye():
+        print('\nThank you for using Bikeshare project')
+        print('Prepared by Reem Alshehri')
+        print('for class: Programming for Data Science')
+        print('Udacity University')
 def main():
 
     while True:
@@ -317,7 +321,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-
+        saying_bye()
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
